63f6bfd0d38abf613c704c247e099aeffd30375a
saif ivna alam <alam.saifivna@gmail.com>
2024-01-22 17:01:57 +0600

    add: app config

---
+++ lib/app_config.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:restaurantmanagement/src/core/show_error.dart';
+
+import 'src/core/app.dart';
+import 'tests/app_tests/test_app.dart';
+
+// ignore_for_file: prefer_final_fields
+
+// ignore_for_file: constant_identifier_names
+
+// ignore_for_file: unnecessary_cast
+
+enum Flavor { TEST, DEV, PROD }
+
+class AppConfig {
+  AppConfig();
+
+  var _isDev = Flavor.DEV;
+  Widget flavour() {
+    Klog.logMessage("Running $_isDev");
+    if (_isDev == Flavor.TEST) {
+      return const TestApp() as Widget;
+    } else {
+      return const App() as Widget;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:restaurantmanagement/src/core/app.dart';
+import 'package:restaurantmanagement/app_config.dart';
 import 'package:restaurantmanagement/src/core/bootstap.dart';
-import 'package:restaurantmanagement/tests/app_tests/test_app.dart';
 
-bool isTesting = false;
 Future<void> main() async {
   await BootStrap().init();
   runApp(
-    isTesting ? const TestApp() : const App(),
+    AppConfig().flavour(),
   );
 }
